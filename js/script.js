6c4679986c34a288493b00c6b84eb9f35f2c6c53
mateuszrosa <mateusz.rosa@gmail.com>
2017-08-09 09:25:37 +0200

    event to displaying div

--- js/script.js
+++ js/script.js
@@ -1,16 +1,12 @@
 $(document).ready(function(){
 
-
 $('.create').on('click', function() {
   $(this).toggleClass('color');
-});
-
-$('.preview').on('click', function() {
-  $(this).toggleClass('color');
-});
-
-$('.export1').on('click', function() {
-  $(this).toggleClass('color');
-});
+    if($('.first').css('display') == 'block') {
+      $('.first').css('display', 'none');
+    } else {
+      $('.first').css('display', 'block');
+    }
+})
 
 })
